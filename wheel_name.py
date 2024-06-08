002de6a4273e7904569fe5c9fcd4b8de580042ff
Nathaniel van Diepen <eeems@eeems.email>
2024-06-08 15:51:40 -0600

    Add missing wheel_name.py

---
+++ wheel_name.py
@@ -0,0 +1,37 @@
+import sys
+import json
+
+from sysconfig import get_platform
+
+
+def get_abi():
+    try:
+        from wheel.pep425tags import get_abi_tag
+
+        return get_abi_tag()
+    except ModuleNotFoundError:
+        pass
+
+    try:
+        from wheel.vendored.packaging import tags
+    except ModuleNotFoundError:
+        from packaging import tags
+
+    name = tags.interpreter_name()
+    version = tags.interpreter_version()
+    return f"{name}{version}"
+
+
+platform = get_platform().replace("-", "_")
+abi = get_abi()
+with open("pyproject.toml", "r") as f:
+    lines = f.read().splitlines()
+
+package = json.loads(
+    [x for x in lines if x.startswith("name = ")][0].split("=")[1].strip()
+)
+version = json.loads(
+    [x for x in lines if x.startswith("version = ")][0].split("=")[1].strip()
+)
+
+sys.stdout.write(f"{package}-{version}-{abi}-{abi}-{platform}.whl")
